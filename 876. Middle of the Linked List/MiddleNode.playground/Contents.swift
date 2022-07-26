60351e65853e1909445d0ec1a3222ac415024e90
Sam <saint00771155@gmail.com>
2022-07-26 13:01:55 +0800

    feat: ModdleNode

--- 876. Middle of the Linked List/MiddleNode.playground/Contents.swift
+++ 876. Middle of the Linked List/MiddleNode.playground/Contents.swift
@@ -42,8 +42,41 @@ func createHeadNode(_ nums: [Int]) -> ListNode {
 }
 
 func middleNode(_ head: ListNode?) -> ListNode? {
-    return nil
+    /// Constraints:
+    /// The number of nodes in the list is in the range [1, 100].
+    /// 1 <= Node.val <= 100
+    var deep = 0
+    var node = head
+    var value: Int = head!.val
+    if node?.next == nil {
+        return node
+    }
+    while ((node?.next) != nil) {
+        value = node!.val
+        node = node?.next
+        deep = deep + 1
+    }
+    guard deep >= 1 && deep <= 100 || value >= 1 && value <= 100 else {
+        return nil
+    }
+    let middle: Int = deep % 2 != 0 ? (deep/2)+1 : deep/2
+    var middleNode: ListNode?
+    var index = 0
+    var currentNode = head
+    while index < middle {
+        currentNode = currentNode?.next
+        middleNode = currentNode
+        index = index + 1
+    }
+    return middleNode
 }
 
 let head1 = createHeadNode([1, 2, 3, 4, 5])
 let head2 = createHeadNode([1, 2, 3, 4, 5, 6])
+let head3 = createHeadNode([2, 3, 4, 5, 6, 7])
+let head4 = createHeadNode([1])
+
+let result1 = middleNode(head1)
+let result2 = middleNode(head2)
+let result3 = middleNode(head3)
+let result4 = middleNode(head4)
