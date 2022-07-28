36442d03ff60d7bcebcbfc5857efabf47fea5683
Sam <saint00771155@gmail.com>
2022-07-28 20:15:47 +0800

    feat: plus one

--- 66. Plus One/PlusOne.playground/Contents.swift
+++ 66. Plus One/PlusOne.playground/Contents.swift
@@ -10,14 +10,14 @@ func plusOne(_ digits: [Int]) -> [Int] {
     var digits = digits
     var index = digits.count - 1
     
-    while index >= 0 {
+    repeat {
         if digits[index] < 9 {
             digits[index] += 1
             return digits
         }
         digits[index] = 0
         index -= 1
-    }
+    } while (index >= 0)
     digits.insert(1, at: 0)
     return digits
 }
@@ -26,8 +26,10 @@ let digits1 = [1,2,3] // Output: [1,2,4]
 let digits2 = [4,3,2,1] // Output: [4,3,2,2]
 let digits3 = [9] // Output: [1,0]
 let digits4 = [7,2,8,5,0,9,1,2,9,5,3,6,6,7,3,2,8,4,3,7,9,5,7,7,4,7,4,9,4,7,0,1,1,1,7,4,0,0,6]
+let digits5 = [9,9,9]
 
 let result1 = plusOne(digits1) // [1,2,4]
 let result2 = plusOne(digits2) // [4,3,2,2]
 let result3 = plusOne(digits3) // [1,0]
 let result4 = plusOne(digits4)
+let result5 = plusOne(digits5)
