b8486746d0c4db95d39bad7d3a5de66b3d6959a9
Sam Lai <sam.lai@tssco.com.tw>
2026-01-21 11:48:11 +0800

    feat: Improve two sum

--- Submitted/Two Sum.playground/Contents.swift
+++ Submitted/Two Sum.playground/Contents.swift
@@ -12,28 +12,53 @@ class Solution {
     }
 }
 
-let solution = Solution()
+class Solution2 {
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        
+        var numbers = nums.enumerated().sorted(by: { $0.element < $1.element })
+        
+        var left = 0
+        var right = numbers.count - 1
+        
+        while left < right {
+            let sum = numbers[left].element + numbers[right].element
+            if sum == target {
+                return [numbers[left].offset, numbers[right].offset]
+            }
+            else if sum < target {
+                left += 1
+            }
+            else {
+                right -= 1
+            }
+        }
+        
+        return []
+    }
+}
+
+let solution = Solution2()
 var nums = [2,7,11,15]
 var target = 9
 var result: [Int] = solution.twoSum(nums, target)
-print(result)
+print("target = \(target), numbers = \(nums), result = \(result)")
 
 nums = [3,2,4]
 target = 6
 result = solution.twoSum(nums, target)
-print(result)
+print("target = \(target), numbers = \(nums), result = \(result)")
 
 nums = [3,3]
 target = 6
 result = solution.twoSum(nums, target)
-print(result)
+print("target = \(target), numbers = \(nums), result = \(result)")
 
 nums = [0,4,3,0]
 target = 0
 result = solution.twoSum(nums, target)
-print(result)
+print("target = \(target), numbers = \(nums), result = \(result)")
 
 nums = [-3,4,3,90]
 target = 0
 result = solution.twoSum(nums, target)
-print(result)
+print("target = \(target), numbers = \(nums), result = \(result)")
