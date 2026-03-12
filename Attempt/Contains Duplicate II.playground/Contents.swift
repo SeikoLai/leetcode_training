e9c7859ed958f59e8388ba11ec9355dd3cba0144
Sam Lai <sam.lai@tssco.com.tw>
2026-03-12 10:58:22 +0800

    feat: Happy Number

--- Attempt/Contains Duplicate II.playground/Contents.swift
+++ Attempt/Contains Duplicate II.playground/Contents.swift
@@ -1,16 +1,30 @@
-import Foundation
-
-// Example 1:
-// Input: nums = [1,2,3,1], k = 3
-// Output: true
-
-// Example 2:
-// Input: nums = [1,0,1,1], k = 1
-// Output: true
-
-// Example 3:
-// Input: nums = [1,2,3,1,2,3], k = 2
-// Output: false
+/**
+ Given an integer array `nums` and an integer `k`, return `true` if there are two distinct indices `i` and `j` in the array such that `nums[i] == nums[j]` and `abs(i - j) <= k`.
+ 
+ 
+ 
+ ``Example 1:``
+ 
+     Input: nums = [1,2,3,1], k = 3
+     Output: true
+ 
+ ``Example 2:``
+ 
+     Input: nums = [1,0,1,1], k = 1
+     Output: true
+ 
+ ``Example 3:``
+ 
+     Input: nums = [1,2,3,1,2,3], k = 2
+     Output: false
+ 
+ 
+ ``Constraints:``
+ 
+ `1 <= nums.length <= 10⁵`
+ `-10⁹ <= nums[i] <= 10⁹`
+ `0 <= k <= 10⁵`
+ */
 
 class Solution {
     func containsNearbyDuplicate(_ nums: [Int], _ k: Int) -> Bool {
